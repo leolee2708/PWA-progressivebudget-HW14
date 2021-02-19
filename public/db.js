d1b7a407e5a87e08123293fb54c5910740374db9
leolee2708 <leho.vietquan@gmail.com>
2021-02-19 17:55:07 -0500

    added db js, manifest and service worker

---
+++ public/db.js
@@ -0,0 +1,61 @@
+var db;
+const request = indexedDb.open('budget', 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+
+    // checking connection status (on or offline)
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function (event) {
+    console.log("Woops!... you encounter this error:" + event.target.errorCode + "!!!");
+};
+
+
+// Create and submit a transaction on the pending db with function saving,read and write
+function saveRecord(record) {
+
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    //add a transaction using add method
+    store.add(record);
+};
+
+
+function checkDatabase() {
+    // looking and access your transaction databases
+    const transactions = db.transaction(["pending"], "readwrite");
+    const store = transactions.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => response.json())
+                .then(() => {
+                    //if sucess, will "write" a transaction onto your pending db 
+                    const transactions = db.transaction(["pending"], "readwrite");
+                    const store = transactions.objectStore("pending");
+                    store.clear();
+                });
+        }
+    };
+};
+
+// checking for app to come online or not
+window.addEventListener("online", checkDatabase);
