0e30624e9bc48b0cbfca1d8bca1506afbe2f4155
leolee2708 <leho.vietquan@gmail.com>
2021-02-19 19:27:27 -0500

    linked to index.html

--- public/service-worker.js
+++ public/service-worker.js
@@ -29,23 +29,23 @@ self.addEventListener("install", function (evt) {
     // has finished installing
       self.skipWaiting();
 });
-// activate
-// self.addEventListener("activate", function(evt) {
-//   evt.waitUntil(
-//     caches.keys().then(keyList => {
-//       return Promise.all(
-//         keyList.map(key => {
-//           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-//             console.log("Removing old cache data", key);
-//             return caches.delete(key);
-//           }
-//         })
-//       );
-//     })
-//   );
+activate
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 
-//   self.clients.claim();
-// });
+  self.clients.claim();
+});
 // fetch
 self.addEventListener("fetch", function (evt) {
     if (evt.request.url.includes("/api/")) {
