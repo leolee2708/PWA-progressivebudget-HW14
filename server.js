1f2526aa3972924e2cdefc6720b279a5e17a2419
leolee2708 <leho.vietquan@gmail.com>
2021-02-22 14:48:54 -0500

    added codes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT|| 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -17,7 +17,10 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+
 });
 
 // routes
